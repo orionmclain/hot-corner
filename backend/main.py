683aa43fe51f35672c932a2e77995a1fadd89e05
orionmclain <orionmclain@gmail.com>
2026-05-26 21:45:19 -0400

    Wired frontend to the Railway backend

--- backend/main.py
+++ backend/main.py
@@ -54,9 +54,9 @@ app.add_middleware(GZipMiddleware, minimum_size=500)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["https://hotcorner.net", "https://www.hotcorner.net", "http://localhost:4200"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"],
 )
 
